d840df3793826eb89f54eba1ac2949d07f6ae457
rajpal229 <rajpalrawat229@gmail.com>
2022-12-28 08:04:20 +0530

    Added: UC1_CreatingContact

---
+++ AddressBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    internal class AddressBook
+    {
+        public string FirstName, LastName, Address, City, State, Email;
+        public int Zip;
+        public long PhoneNumber;
+        public AddressBook(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Email = email;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+        }
+        public void DisplayContact()
+        {
+            Console.WriteLine("Name {0} {1},", FirstName, LastName);
+            Console.WriteLine("Adrdess {0} ", Address);
+            Console.WriteLine("City {0}", City);
+            Console.WriteLine("State {0} ", State);
+            Console.WriteLine("Email {0}", Email);
+            Console.WriteLine("Zip {0}", Zip);
+            Console.WriteLine("PhoneNumber {0}", PhoneNumber);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using AddressBookProgram;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address BOOK Program");
+            AddressBook Add = new AddressBook("Rajpal", "Singh", "Ramesh Nagar", "Delhi", "New Delhi", "rajpalsinghrawat229@gamil.com", 110015, 9050545979);
+                Add.DisplayContact();
+        }
+    }
+}
